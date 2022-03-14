53b60ca3722b446e11a0e13f0c1aa07d6c8c3d67
Petr Bartos <petr.bartos@plus4u.net>
2022-03-14 17:36:35 +0100

    Update linux build script

--- android/run.sh
+++ android/run.sh
@@ -1,8 +1,23 @@
-#!/bin/sh
-
-cd ..
+#!/bin/bash
 
 TARGET=debug
+
+WORKDIR=$(readlink -f $(dirname $0))
+
+NAME=$(cat $WORKDIR/app/src/main/res/values/strings.xml | grep "app_name" | cut -d">" -f2 | cut -d"<" -f1 )
+VERSION=$(cat $WORKDIR/app/src/main/AndroidManifest.xml | grep "versionName" | cut -d\" -f2 | cut -d\" -f1 )
+PACKAGE_NAME=$(cat $WORKDIR/app/src/main/AndroidManifest.xml | grep "package" | cut -d\" -f2 | cut -d\" -f1 )
+
+if [ "$1" == "clean" ] || [ "$2" == "clean" ] || [ "$3" == "clean" ]; then
+	rm -rf $WORKDIR/../build
+	rm -rf $WORKDIR/build
+	rm -rf $WORKDIR/app/build
+	rm -rf $WORKDIR/app/src/main/jniLibs
+fi
+
+echo "#define Q3QVERSION  \"$VERSION"\" > $WORKDIR/app/src/main/cpp/code/vr/vr_version.h
+
+cd $WORKDIR/..
 
 make -j $(getconf _NPROCESSORS_ONLN) $TARGET
 if [ $? -ne 0 ]; then
@@ -10,7 +25,7 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-cd android
+cd $WORKDIR
 
 ./make.sh
 
@@ -19,19 +34,21 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-PACKAGE_NAME=com.drbeef.ioq3quest
 ANDROID_STORAGE_LOCATION=/sdcard/Android/data/$PACKAGE_NAME/files/
-APK_LOCATION=./app/build/outputs/apk/debug/app-debug.apk
+APK_LOCATION=$WORKDIR/app/build/outputs/apk/debug/app-debug.apk
 
-adb install -r $APK_LOCATION
-if [ $? -ne 0 ]; then
-	adb uninstall $PACKAGE_NAME
-	adb install $APK_LOCATION
-	if [ $? -ne 0 ]; then
-		echo "Failed to install apk."
-		exit 1
-	fi
+if [ "$1" == "install" ] || [ "$2" == "install" ] || [ "$3" == "install" ]; then
+  adb install -r $APK_LOCATION
+  if [ $? -ne 0 ]; then
+    adb uninstall $PACKAGE_NAME
+    adb install $APK_LOCATION
+    if [ $? -ne 0 ]; then
+      echo "Failed to install apk."
+      exit 1
+    fi
+  fi
 fi
+
 #adb shell mkdir -p $ANDROID_STORAGE_LOCATION
 #adb push --sync ~/.local/share/Steam/steamapps/common/Quake\ 3\ Arena/baseq3 $ANDROID_STORAGE_LOCATION
 #if [ $? -ne 0 ]; then
@@ -49,10 +66,12 @@ fi
 #	exit 1
 #fi
 
-adb logcat -c
-adb shell am start -n $PACKAGE_NAME/.MainActivity
-if [ $? -ne 0 ]; then
-	echo "Failed to start application."
-	exit 1
+if [ "$1" == "start" ] || [ "$2" == "start" ] || [ "$3" == "start" ]; then
+  adb logcat -c
+  adb shell am start -n $PACKAGE_NAME/.MainActivity
+  if [ $? -ne 0 ]; then
+    echo "Failed to start application."
+    exit 1
+  fi
+  adb logcat *:S Quake3:V SDL:V DEBUG:V
 fi
-adb logcat *:S Quake3:V SDL:V DEBUG:V
