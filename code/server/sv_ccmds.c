5c791e9bc9ce3190c2e1f50d288819c1e61422a8
Peter Donald <peter@realityforge.org>
2022-03-15 16:51:59 +1100

    Remove PRE_RELEASE_DEMO define

--- code/server/sv_ccmds.c
+++ code/server/sv_ccmds.c
@@ -615,11 +615,6 @@ void SV_AddOperatorCommands( void ) {
 	Cmd_AddCommand ("map_restart", SV_MapRestart_f);
 	Cmd_AddCommand ("sectorlist", SV_SectorList_f);
 	Cmd_AddCommand ("map", SV_Map_f);
-#ifndef PRE_RELEASE_DEMO
-	Cmd_AddCommand ("devmap", SV_Map_f);
-	Cmd_AddCommand ("spmap", SV_Map_f);
-	Cmd_AddCommand ("spdevmap", SV_Map_f);
-#endif
 	Cmd_AddCommand ("killserver", SV_KillServer_f);
 	if( com_dedicated->integer ) {
 		Cmd_AddCommand ("say", SV_ConSay_f);
