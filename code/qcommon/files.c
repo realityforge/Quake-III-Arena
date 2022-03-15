fd374d5fb6ebef272e1c3916ef6cc963187fb08b
Peter Donald <peter@realityforge.org>
2022-03-15 15:09:44 +1100

    Remove NULL sound module by omitting calls to S_ClearSoundBuffer when building DEDICATED variant

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -682,7 +682,9 @@ long FS_SV_FOpenFileRead(const char *filename, fileHandle_t *fp)
 	Q_strncpyz( fsh[f].name, filename, sizeof( fsh[f].name ) );
 
 	// don't let sound stutter
+#ifndef DEDICATED
 	S_ClearSoundBuffer();
+#endif
 
 	// search homepath
 	ospath = FS_BuildOSPath( fs_homepath->string, filename, "" );
@@ -741,8 +743,10 @@ void FS_SV_Rename( const char *from, const char *to, qboolean safe ) {
 		Com_Error( ERR_FATAL, "Filesystem call made without initialization" );
 	}
 
+#ifndef DEDICATED
 	// don't let sound stutter
 	S_ClearSoundBuffer();
+#endif
 
 	from_ospath = FS_BuildOSPath( fs_homepath->string, from, "" );
 	to_ospath = FS_BuildOSPath( fs_homepath->string, to, "" );
@@ -775,8 +779,10 @@ void FS_Rename( const char *from, const char *to ) {
 		Com_Error( ERR_FATAL, "Filesystem call made without initialization" );
 	}
 
+#ifndef DEDICATED
 	// don't let sound stutter
 	S_ClearSoundBuffer();
+#endif
 
 	from_ospath = FS_BuildOSPath( fs_homepath->string, fs_gamedir, from );
 	to_ospath = FS_BuildOSPath( fs_homepath->string, fs_gamedir, to );
@@ -883,8 +889,10 @@ fileHandle_t FS_FOpenFileAppend( const char *filename ) {
 
 	Q_strncpyz( fsh[f].name, filename, sizeof( fsh[f].name ) );
 
+#ifndef DEDICATED
 	// don't let sound stutter
 	S_ClearSoundBuffer();
+#endif
 
 	ospath = FS_BuildOSPath( fs_homepath->string, fs_gamedir, filename );
 
@@ -926,8 +934,10 @@ fileHandle_t FS_FCreateOpenPipeFile( const char *filename ) {
 
 	Q_strncpyz( fsh[f].name, filename, sizeof( fsh[f].name ) );
 
+#ifndef DEDICATED
 	// don't let sound stutter
 	S_ClearSoundBuffer();
+#endif
 
 	ospath = FS_BuildOSPath( fs_homepath->string, fs_gamedir, filename );
 
