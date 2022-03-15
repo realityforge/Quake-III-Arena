c419e9619c9c19b68eb1d06c6900f3bc21e3546a
Peter Donald <peter@realityforge.org>
2022-03-15 22:48:08 +1100

    Remove unused prototype

--- code/server/server.h
+++ code/server/server.h
@@ -305,7 +305,6 @@ void SV_Heartbeat_f( void );
 //
 void SV_AddServerCommand( client_t *client, const char *cmd );
 void SV_UpdateServerCommandsToClient( client_t *client, msg_t *msg );
-void SV_WriteFrameToClient (client_t *client, msg_t *msg);
 void SV_SendMessageToClient( msg_t *msg, client_t *client );
 void SV_SendClientMessages( void );
 void SV_SendClientSnapshot( client_t *client );
