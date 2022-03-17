dcf642fa3a37e3bf1dcb4f3ff1b667a3b7186c4c
Peter Donald <peter@realityforge.org>
2022-03-17 16:49:04 +1100

    Use typedef'ed usercmd_t for clarity

--- code/qcommon/qcommon.h
+++ code/qcommon/qcommon.h
@@ -54,7 +54,6 @@ void MSG_Bitstream( msg_t *buf );
 // sets data buffer as MSG_Init does prior to do the copy
 void MSG_Copy(msg_t *buf, byte *data, int length, msg_t *src);
 
-struct usercmd_s;
 struct entityState_s;
 struct playerState_s;
 
@@ -86,8 +85,8 @@ float	MSG_ReadAngle16 (msg_t *sb);
 void	MSG_ReadData (msg_t *sb, void *buffer, int size);
 
 
-void MSG_WriteDeltaUsercmd( msg_t *msg, struct usercmd_s *from, struct usercmd_s *to );
-void MSG_ReadDeltaUsercmd( msg_t *msg, struct usercmd_s *from, struct usercmd_s *to );
+void MSG_WriteDeltaUsercmd( msg_t *msg, usercmd_t *from, usercmd_t *to );
+void MSG_ReadDeltaUsercmd( msg_t *msg, usercmd_t *from, usercmd_t *to );
 
 void MSG_WriteDeltaUsercmdKey( msg_t *msg, int key, usercmd_t *from, usercmd_t *to );
 void MSG_ReadDeltaUsercmdKey( msg_t *msg, int key, usercmd_t *from, usercmd_t *to );
