955e120859ca3e6b16e0e44096b989607bb316aa
Peter Donald <peter@realityforge.org>
2022-03-17 14:30:54 +1100

    Revert "Remove PRE_RELEASE_DEMO define"
    This reverts commit e6de78f8deaf8d5291f3f98a0743865d4f967d1e.

--- code/qcommon/qcommon.h
+++ code/qcommon/qcommon.h
@@ -33,6 +33,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 #endif
 
+//#define	PRE_RELEASE_DEMO
+
 //============================================================================
 
 //
