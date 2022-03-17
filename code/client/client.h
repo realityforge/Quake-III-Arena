07578d0575f4928774e274bb6c421d0fa07e475f
Peter Donald <peter@realityforge.org>
2022-03-17 16:34:06 +1100

    Remove unused prototype

--- code/client/client.h
+++ code/client/client.h
@@ -496,8 +496,6 @@ void CL_ReadPackets (void);
 void CL_WritePacket( void );
 void IN_CenterView (void);
 
-void CL_VerifyCode( void );
-
 float CL_KeyState (kbutton_t *key);
 int Key_StringToKeynum( char *str );
 char *Key_KeynumToString (int keynum);
