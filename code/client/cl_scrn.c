0c1a0201010f2d01bb40a77d5f82d99887de9cb6
Peter Donald <peter@realityforge.org>
2022-03-17 16:30:14 +1100

    Remove unused function

--- code/client/cl_scrn.c
+++ code/client/cl_scrn.c
@@ -33,24 +33,6 @@ cvar_t		*cl_graphshift;
 
 /*
 ================
-SCR_DrawNamedPic
-
-Coordinates are 640*480 virtual values
-=================
-*/
-void SCR_DrawNamedPic( float x, float y, float width, float height, const char *picname ) {
-	qhandle_t	hShader;
-
-	assert( width != 0 );
-
-	hShader = re.RegisterShader( picname );
-	SCR_AdjustFrom640( &x, &y, &width, &height );
-	re.DrawStretchPic( x, y, width, height, 0, 0, 1, 1, hShader );
-}
-
-
-/*
-================
 SCR_AdjustFrom640
 
 Adjusted for resolution and screen aspect ratio
