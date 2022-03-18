d9255b57ce7ff10961c51aa94685109b83cbde30
Peter Donald <peter@realityforge.org>
2022-03-18 14:50:19 +1100

    Remove unused constants

--- code/sys/win_resource.h
+++ code/sys/win_resource.h
@@ -25,11 +25,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 #define IDS_STRING1                     1
 #define IDI_ICON1                       1
-#define IDB_BITMAP1                     1
-#define IDB_BITMAP2                     128
-#define IDC_CURSOR1                     129
-#define IDC_CURSOR2                     130
-#define IDC_CURSOR3                     131
 
 // Next default values for new objects
 // 
