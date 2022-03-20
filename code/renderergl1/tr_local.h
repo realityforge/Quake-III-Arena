51951ae0eb6035b3a06c58e3b2b622c031c1dd37
Peter Donald <peter@realityforge.org>
2022-03-20 23:57:41 +1100

    Remove unused prototype

--- code/renderergl1/tr_local.h
+++ code/renderergl1/tr_local.h
@@ -1289,7 +1289,6 @@ void RB_ProjectionShadowDeform( void );
 
 void R_BuildCloudData( shaderCommands_t *shader );
 void R_InitSkyTexCoords( float cloudLayerHeight );
-void R_DrawSkyBox( shaderCommands_t *shader );
 void RB_DrawSun( float scale, shader_t *shader );
 void RB_ClipSkyPolygons( shaderCommands_t *shader );
 
