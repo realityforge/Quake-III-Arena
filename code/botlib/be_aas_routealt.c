f640018ce87fbd03f0f69ed9124f5e968f917c53
Peter Donald <peter@realityforge.org>
2022-03-22 07:37:50 +1100

    Remove unused define

--- code/botlib/be_aas_routealt.c
+++ code/botlib/be_aas_routealt.c
@@ -34,7 +34,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "be_interface.h"
 #include "be_aas_def.h"
 
-#define ENABLE_ALTROUTING
 //#define ALTROUTE_DEBUG
 
 typedef struct midrangearea_s
