954f1432d9088a3b3fb3079b48113194588e3b85
Peter Donald <peter@realityforge.org>
2022-03-26 17:04:21 +1100

    Remove unused function FS_Rename

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -686,34 +686,6 @@ void FS_SV_Rename( const char *from, const char *to ) {
 	}
 }
 
-
-
-void FS_Rename( const char *from, const char *to ) {
-	char			*from_ospath, *to_ospath;
-
-	if ( !fs_searchpaths ) {
-		Com_Error( ERR_FATAL, "Filesystem call made without initialization\n" );
-	}
-
-#ifndef DEDICATED
-	// don't let sound stutter
-	S_ClearSoundBuffer();
-#endif
-
-	from_ospath = FS_BuildOSPath( fs_homepath->string, fs_gamedir, from );
-	to_ospath = FS_BuildOSPath( fs_homepath->string, fs_gamedir, to );
-
-	if ( fs_debug->integer ) {
-		Com_Printf( "FS_Rename: %s --> %s\n", from_ospath, to_ospath );
-	}
-
-	if (rename( from_ospath, to_ospath )) {
-		// Failed, try copying it and deleting the original
-		FS_CopyFile ( from_ospath, to_ospath );
-		FS_Remove ( from_ospath );
-	}
-}
-
 /*
 ==============
 FS_FCloseFile
