892e176c4e08b05813f191255ff9ec387ce1cfb0
Peter Donald <peter@realityforge.org>
2022-03-27 08:27:07 +1100

    Remove variable extratraveltime that was never read

--- code/botlib/be_aas_routealt.c
+++ code/botlib/be_aas_routealt.c
@@ -164,9 +164,6 @@ int AAS_AlternativeRouteGoals(vec3_t start, int startareanum, vec3_t goal, int g
 		VectorCopy(aasworld.areas[bestareanum].center, altroutegoals[numaltroutegoals].origin);
 		altroutegoals[numaltroutegoals].areanum = bestareanum;
 		altroutegoals[numaltroutegoals].starttraveltime = midrangeareas[bestareanum].starttime;
-		altroutegoals[numaltroutegoals].extratraveltime =
-					(midrangeareas[bestareanum].starttime + midrangeareas[bestareanum].goaltime) -
-								goaltraveltime;
 		numaltroutegoals++;
 		//
 #ifdef ALTROUTE_DEBUG
