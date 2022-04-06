2b57178db24e74c366f84870348af8b0d11be9d1
Peter Donald <peter@realityforge.org>
2022-04-07 09:51:08 +1000

    Remove historic remnants of memset and memcpy

--- code/qcommon/md4.c
+++ code/qcommon/md4.c
@@ -38,14 +38,6 @@ void MD4Init (MD4_CTX *);
 void MD4Update (MD4_CTX *, const unsigned char *, unsigned int);
 void MD4Final (unsigned char [16], MD4_CTX *);
 
-#ifndef __VECTORC  
-void memset (void* dest, const int val, const size_t count);
-void memcpy (void* dest, const void* src, const size_t count);
-#else
-#define memset memset
-#define memcpy memcpy
-#endif
-
 /* MD4C.C - RSA Data Security, Inc., MD4 message-digest algorithm */
 /* Copyright (C) 1990-2, RSA Data Security, Inc. All rights reserved.
   
