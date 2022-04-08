feccc211309f821f2fdf043ea3ec6cc7c11d870e
Peter Donald <peter@realityforge.org>
2022-04-09 07:22:08 +1000

    Decruft

--- code/game/ai_vcmd.c
+++ code/game/ai_vcmd.c
@@ -30,7 +30,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "be_ai_goal.h"
 #include "be_ai_move.h"
 #include "be_ai_weap.h"
-//
 #include "ai_main.h"
 #include "ai_dmq3.h"
 #include "ai_chat.h"
@@ -38,7 +37,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "ai_dmnet.h"
 #include "ai_team.h"
 #include "ai_vcmd.h"
-//
 #include "chars.h"				//characteristics
 #include "inv.h"				//indexes into the inventory
 #include "syn.h"				//synonyms
