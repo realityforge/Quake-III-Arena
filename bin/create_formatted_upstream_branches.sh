30c424bb67abd37fd5c58cec66095f29eb6e89df
Peter Donald <peter@realityforge.org>
2022-04-12 09:22:40 +1000

    Add formatted version of ioq3quest

--- bin/create_formatted_upstream_branches.sh
+++ bin/create_formatted_upstream_branches.sh
@@ -7,6 +7,31 @@ export BRANCH_WITH_CLANG_FORMAT=origin/BazelBuild
 # Fetch origin to ensure the .clang-format is present and updated
 git fetch origin
 
+git checkout upstream/ioq3quest
+git pull
+git branch -D upstream/ioq3quest_formatted 2> /dev/null > /dev/null || echo "No Local branch, creating local branch and formatting"
+git checkout -B upstream/ioq3quest_formatted
+git show ${BRANCH_WITH_CLANG_FORMAT}:.clang-format > .clang-format
+# shellcheck disable=SC2038
+find android/app/src/main/cpp/code \
+     \( -name '*.h' -or -name '*.c' \)  \
+     ! -path 'android/app/src/main/cpp/code/AL/*' -and \
+     ! -path 'android/app/src/main/cpp/code/curl-7.54.0/*' -and \
+     ! -path 'android/app/src/main/cpp/code/jpeg-8c/*' -and \
+     ! -path 'android/app/src/main/cpp/code/libogg-1.3.3/*' -and \
+     ! -path 'android/app/src/main/cpp/code/libvorbis-1.3.6/*' -and \
+     ! -path 'android/app/src/main/cpp/code/opus-1.2.1/*' -and \
+     ! -path 'android/app/src/main/cpp/code/opusfile-0.9/*' -and \
+     ! -path 'android/app/src/main/cpp/code/SDL2/*' -and \
+     ! -path 'android/app/src/main/cpp/code/tools/*' -and \
+     ! -path 'android/app/src/main/cpp/code/zlib/*' \
+     | xargs clang-format -i
+git add android/app/src/main/cpp/code/
+rm .clang-format
+git commit -m "Format the source code with clang-format to simplify cross-branch comparisons"
+git push origin :upstream/ioq3quest_formatted 2> /dev/null > /dev/null || echo "No upstream/ioq3quest_formatted branch to delete"
+git push -f --set-upstream origin upstream/ioq3quest_formatted
+
 git checkout upstream/ioq3
 git pull
 git branch -D upstream/ioq3_formatted 2> /dev/null > /dev/null || echo "No Local branch, creating local branch and formatting"
