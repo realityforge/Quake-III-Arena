d60128cf21087b5d8a45f8a7d1ceabbe77bd4324
Simon <simonbrown77@googlemail.com>
2022-04-27 17:58:22 +0100

    Version number updated

--- android/run.bat
+++ android/run.bat
@@ -3,7 +3,7 @@
 setlocal
 
 set BUILD_TYPE=release
-set VERSION=0.31.2-multiview
+set VERSION=0.31.3-multiview
 
 @REM Define the following environment variables to sign a release build
 @REM set KEYSTORE=
