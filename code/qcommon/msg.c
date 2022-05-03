5c58cd2af67787a7a2a0e95eae6a433d74623e00
Peter Donald <peter@realityforge.org>
2022-05-04 09:14:57 +1000

    Remove some unused methods

--- code/qcommon/msg.c
+++ code/qcommon/msg.c
@@ -495,45 +495,6 @@ extern cvar_t* cl_shownet;
 #define LOG(x)
 #endif
 
-void MSG_WriteDelta(msg_t* msg, int oldV, int newV, int bits)
-{
-    if (oldV == newV) {
-        MSG_WriteBits(msg, 0, 1);
-        return;
-    }
-    MSG_WriteBits(msg, 1, 1);
-    MSG_WriteBits(msg, newV, bits);
-}
-
-int MSG_ReadDelta(msg_t* msg, int oldV, int bits)
-{
-    if (MSG_ReadBits(msg, 1)) {
-        return MSG_ReadBits(msg, bits);
-    }
-    return oldV;
-}
-
-void MSG_WriteDeltaFloat(msg_t* msg, float oldV, float newV)
-{
-    if (oldV == newV) {
-        MSG_WriteBits(msg, 0, 1);
-        return;
-    }
-    MSG_WriteBits(msg, 1, 1);
-    MSG_WriteBits(msg, *(int*)&newV, 32);
-}
-
-float MSG_ReadDeltaFloat(msg_t* msg, float oldV)
-{
-    if (MSG_ReadBits(msg, 1)) {
-        float newV;
-
-        *(int*)&newV = MSG_ReadBits(msg, 32);
-        return newV;
-    }
-    return oldV;
-}
-
 /*
 =============================================================================
 
