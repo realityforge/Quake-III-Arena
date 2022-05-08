6ef89a460f087cffb24e3988d05d3efa5e634a44
Peter Donald <peter@realityforge.org>
2022-05-09 09:04:34 +1000

    Code cleanup

--- code/renderer/tr_noise.c
+++ code/renderer/tr_noise.c
@@ -32,37 +32,37 @@ static int s_noise_perm[NOISE_SIZE];
 
 static float GetNoiseValue(int x, int y, int z, int t)
 {
-    int index = INDEX((int)x, (int)y, (int)z, (int)t);
-
-    return s_noise_table[index];
+    return s_noise_table[INDEX(x, y, z, t)];
 }
 
 void R_NoiseInit(void)
 {
-    int i;
-
-    for (i = 0; i < NOISE_SIZE; i++) {
-        s_noise_table[i] = (float)(((rand() / (float)RAND_MAX) * 2.0 - 1.0));
-        s_noise_perm[i] = (unsigned char)(rand() / (float)RAND_MAX * 255);
+    for (int i = 0; i < NOISE_SIZE; i++) {
+        s_noise_table[i] = (float)((((float)(rand()) / (float)RAND_MAX) * 2.0F - 1.0F));
+        s_noise_perm[i] = (unsigned char)((float)(rand()) / (float)RAND_MAX * 255.0F);
     }
 }
 
 float R_NoiseGet4f(float x, float y, float z, double t)
 {
-    float front[4];
-    float back[4];
-    float fvalue, bvalue, value[2], finalvalue;
-
-    const int ix = (int)floorf(x);
+    const float ixf = floorf(x);
+    const int ix = (int)ixf;
     const float fx = x - (float)ix;
-    const int iy = (int)floorf(y);
+    const float iyf = floorf(y);
+    const int iy = (int)iyf;
     const float fy = y - (float)iy;
-    const int iz = (int)floorf(z);
+    const float izf = floorf(z);
+    const int iz = (int)izf;
     const float fz = z - (float)iz;
-    const int it = (int)floor(t);
+    const double itd = floor(t);
+    const int it = (int)itd;
     const float ft = (float)(t - (double)it);
 
+    float value[2];
     for (int i = 0; i < 2; i++) {
+        float front[4];
+        float back[4];
+
         front[0] = GetNoiseValue(ix, iy, iz, it + i);
         front[1] = GetNoiseValue(ix + 1, iy, iz, it + i);
         front[2] = GetNoiseValue(ix, iy + 1, iz, it + i);
@@ -73,13 +73,10 @@ float R_NoiseGet4f(float x, float y, float z, double t)
         back[2] = GetNoiseValue(ix, iy + 1, iz + 1, it + i);
         back[3] = GetNoiseValue(ix + 1, iy + 1, iz + 1, it + i);
 
-        fvalue = LERP(LERP(front[0], front[1], fx), LERP(front[2], front[3], fx), fy);
-        bvalue = LERP(LERP(back[0], back[1], fx), LERP(back[2], back[3], fx), fy);
+        const float fvalue = LERP(LERP(front[0], front[1], fx), LERP(front[2], front[3], fx), fy);
+        const float bvalue = LERP(LERP(back[0], back[1], fx), LERP(back[2], back[3], fx), fy);
 
         value[i] = LERP(fvalue, bvalue, fz);
     }
-
-    finalvalue = LERP(value[0], value[1], ft);
-
-    return finalvalue;
+    return LERP(value[0], value[1], ft);
 }
