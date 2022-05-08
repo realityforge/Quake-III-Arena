29fea8e9291307a19152826d921ce2a130a247b5
Peter Donald <peter@realityforge.org>
2022-05-08 23:51:09 +1000

    Remove unnecessary comment

--- code/renderer/tr_noise.c
+++ code/renderer/tr_noise.c
@@ -19,7 +19,6 @@ along with Quake III Arena source code; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
-// tr_noise.c
 #include "tr_common.h"
 
 #define NOISE_SIZE 256
