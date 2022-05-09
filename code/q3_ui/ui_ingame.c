fcf81329012cc93ae3fe705222513ee019aad141
Peter Donald <peter@realityforge.org>
2022-05-09 19:29:27 +1000

    Remove commented code

--- code/q3_ui/ui_ingame.c
+++ code/q3_ui/ui_ingame.c
@@ -31,7 +31,6 @@ INGAME MENU
 #include "ui_local.h"
 
 #define INGAME_FRAME "menu/art/addbotframe"
-//#define INGAME_FRAME					"menu/art/cut_frame"
 #define INGAME_MENU_VERTICAL_SPACING 28
 
 #define ID_TEAM 10
