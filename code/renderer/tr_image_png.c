49bca16f08ebb35490e9367d2db82ecd7ed1f635
Peter Donald <peter@realityforge.org>
2022-05-09 16:37:06 +1000

    Rename method for clarity

--- code/renderer/tr_image_png.c
+++ code/renderer/tr_image_png.c
@@ -107,7 +107,7 @@ static void r_spng_load_error(const char* name, const int result, const char* fu
     ri.Printf(PRINT_WARNING, "R_LoadPNG: Failed to load png file named %s due to %s error calling %s.\n", name, spng_strerror(result), functionName);
 }
 
-static qboolean R_LoadPNGFromBuffer(const char* name, const void* buffer, const long buffer_size, image_load_result_t* image_load_result)
+static qboolean R_DecodePNGinBuffer(const char* name, const void* buffer, const long buffer_size, image_load_result_t* image_load_result)
 {
     struct spng_ihdr ihdr;
     void* image = NULL;
@@ -200,7 +200,7 @@ void R_LoadPNG(const char* name, byte** pixel_data, uint32_t* image_width, uint3
         goto cleanup;
     } else {
         image_load_result_t image_load_result;
-        if (qtrue == R_LoadPNGFromBuffer(name_to_request, asset_data, asset_size, &image_load_result)) {
+        if (qtrue == R_DecodePNGinBuffer(name_to_request, asset_data, asset_size, &image_load_result)) {
             *image_width = image_load_result.image_width;
             *image_height = image_load_result.image_height;
             *pixel_data = image_load_result.pixel_data;
