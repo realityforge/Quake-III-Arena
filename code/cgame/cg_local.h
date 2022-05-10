9c7d22eaf3ddcd2012584f1ac624037fe3978a9b
Peter Donald <peter@realityforge.org>
2022-05-10 21:05:22 +1000

    Decruft

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -19,7 +19,6 @@ along with Quake III Arena source code; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
-//
 #include "../qcommon/attributes.h"
 #include "../game/q_shared.h"
 #include "tr_types.h"
