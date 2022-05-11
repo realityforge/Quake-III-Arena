9be2c8d66c16f585ef693de006d7c1d618417340
Peter Donald <peter@realityforge.org>
2022-05-11 11:31:54 +1000

    Simplify code and restructure

--- code/renderer/tr_image.c
+++ code/renderer/tr_image.c
@@ -304,38 +304,35 @@ If a larger shrinking is needed, use the mipmap function
 before or after.
 ================
 */
-static void ResampleTexture(const byte* in, const int inwidth, const int inheight, byte* out, int outwidth, int outheight)
+static void ResampleTexture(const byte* in, const int inwidth, const int inheight, byte* out, const int outwidth, const int outheight)
 {
-    int i, j;
-    byte *inrow, *inrow2;
-    int frac, fracstep;
     int p1[2048], p2[2048];
-    byte *pix1, *pix2, *pix3, *pix4;
 
-    if (outwidth > 2048)
+    if (outwidth > 2048) {
         ri.Error(ERR_DROP, "ResampleTexture: max width");
+    }
 
-    fracstep = inwidth * 0x10000 / outwidth;
+    const int fracstep = inwidth * 0x10000 / outwidth;
 
-    frac = fracstep >> 2;
-    for (i = 0; i < outwidth; i++) {
+    int frac = fracstep >> 2;
+    for (int i = 0; i < outwidth; i++) {
         p1[i] = 4 * (frac >> 16);
         frac += fracstep;
     }
     frac = 3 * (fracstep >> 2);
-    for (i = 0; i < outwidth; i++) {
+    for (int i = 0; i < outwidth; i++) {
         p2[i] = 4 * (frac >> 16);
         frac += fracstep;
     }
 
-    for (i = 0; i < outheight; i++) {
-        inrow = in + 4 * inwidth * (int)((i + 0.25) * inheight / outheight);
-        inrow2 = in + 4 * inwidth * (int)((i + 0.75) * inheight / outheight);
-        for (j = 0; j < outwidth; j++) {
-            pix1 = inrow + p1[j];
-            pix2 = inrow + p2[j];
-            pix3 = inrow2 + p1[j];
-            pix4 = inrow2 + p2[j];
+    for (int i = 0; i < outheight; i++) {
+        const byte* inrow = in + 4 * inwidth * (int)((i + 0.25) * inheight / outheight);
+        const byte* inrow2 = in + 4 * inwidth * (int)((i + 0.75) * inheight / outheight);
+        for (int j = 0; j < outwidth; j++) {
+            const byte* pix1 = inrow + p1[j];
+            const byte* pix2 = inrow + p2[j];
+            const byte* pix3 = inrow2 + p1[j];
+            const byte* pix4 = inrow2 + p2[j];
             *out++ = (pix1[0] + pix2[0] + pix3[0] + pix4[0]) >> 2;
             *out++ = (pix1[1] + pix2[1] + pix3[1] + pix4[1]) >> 2;
             *out++ = (pix1[2] + pix2[2] + pix3[2] + pix4[2]) >> 2;
