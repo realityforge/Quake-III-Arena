2d790cfd5ad4e4415a344e7d53bb9ad7da401ff9
Peter Donald <peter@realityforge.org>
2022-05-13 11:23:42 +1000

    Add autoformatting of shell scripts

--- bin/setup_ioq3_upstream.sh
+++ bin/setup_ioq3_upstream.sh
@@ -26,12 +26,12 @@ export BRANCH_WITH_CLANG_FORMAT=origin/master
 # Fetch origin to ensure the .clang-format is present and updated
 git fetch origin
 
-git remote rm ioq3 2> /dev/null > /dev/null || (echo "ioq3 remote not present")
+git remote rm ioq3 2>/dev/null >/dev/null || (echo "ioq3 remote not present")
 git remote add --no-tags --track main ioq3 https://github.com/ioquake/ioq3.git
 git fetch ioq3
 
 git checkout ioq3/main
-git branch -D upstream/ioq3 2> /dev/null > /dev/null || (echo "ioq3 upstream branch not present")
+git branch -D upstream/ioq3 2>/dev/null >/dev/null || (echo "ioq3 upstream branch not present")
 git checkout -B upstream/ioq3
 git config branch.upstream/ioq3.remote ioq3
 git config branch.upstream/ioq3.merge refs/heads/main
@@ -41,28 +41,28 @@ git commit -m "Remove workflows from upstream branch to avoid executing actions 
 git gc --prune=now --aggressive
 git push -f
 
-git branch -D upstream/ioq3_formatted 2> /dev/null > /dev/null || echo "No Local branch, creating local branch and formatting"
+git branch -D upstream/ioq3_formatted 2>/dev/null >/dev/null || echo "No Local branch, creating local branch and formatting"
 git checkout -B upstream/ioq3_formatted
 pwd
-git show ${BRANCH_WITH_CLANG_FORMAT}:.clang-format > .clang-format
+git show ${BRANCH_WITH_CLANG_FORMAT}:.clang-format >.clang-format
 # shellcheck disable=SC2038
 find code \
-     \( -name '*.h' -or -name '*.c' -or -name '*.m' \)  \
-     ! -path 'code/AL/*' -and \
-     ! -path 'code/curl-7.54.0/*' -and \
-     ! -path 'code/jpeg-8c/*' -and \
-     ! -path 'code/libogg-1.3.3/*' -and \
-     ! -path 'code/libvorbis-1.3.6/*' -and \
-     ! -path 'code/opus-1.2.1/*' -and \
-     ! -path 'code/opusfile-0.9/*' -and \
-     ! -path 'code/SDL2/*' -and \
-     ! -path 'code/tools/*' -and \
-     ! -path 'code/zlib/*' \
-     | xargs clang-format -i
+    \( -name '*.h' -or -name '*.c' -or -name '*.m' \) \
+    ! -path 'code/AL/*' -and \
+    ! -path 'code/curl-7.54.0/*' -and \
+    ! -path 'code/jpeg-8c/*' -and \
+    ! -path 'code/libogg-1.3.3/*' -and \
+    ! -path 'code/libvorbis-1.3.6/*' -and \
+    ! -path 'code/opus-1.2.1/*' -and \
+    ! -path 'code/opusfile-0.9/*' -and \
+    ! -path 'code/SDL2/*' -and \
+    ! -path 'code/tools/*' -and \
+    ! -path 'code/zlib/*' |
+    xargs clang-format -i
 git add code/
 rm .clang-format
 git commit -m "Format the source code with clang-format to simplify cross-branch comparisons"
-git push origin :upstream/ioq3_formatted 2> /dev/null > /dev/null || echo "No upstream/ioq3_formatted branch to delete"
+git push origin :upstream/ioq3_formatted 2>/dev/null >/dev/null || echo "No upstream/ioq3_formatted branch to delete"
 git push -f --set-upstream origin upstream/ioq3_formatted
 
 # Return to master branch on completion
--- bin/setup_quake3e_upstream.sh
+++ bin/setup_quake3e_upstream.sh
@@ -26,12 +26,12 @@ export BRANCH_WITH_CLANG_FORMAT=origin/master
 # Fetch origin to ensure the .clang-format is present and updated
 git fetch origin
 
-git remote rm quake3e 2> /dev/null > /dev/null || (echo "quake3e remote not present")
+git remote rm quake3e 2>/dev/null >/dev/null || (echo "quake3e remote not present")
 git remote add --no-tags --track master quake3e https://github.com/ec-/Quake3e.git
 git fetch quake3e
 
 git checkout quake3e/master
-git branch -D upstream/quake3e 2> /dev/null > /dev/null || (echo "quake3e upstream branch not present")
+git branch -D upstream/quake3e 2>/dev/null >/dev/null || (echo "quake3e upstream branch not present")
 git checkout -B upstream/quake3e
 git config branch.upstream/quake3e.remote quake3e
 git config branch.upstream/quake3e.merge refs/heads/master
@@ -41,17 +41,17 @@ git commit -m "Remove workflows from upstream branch to avoid executing actions 
 git gc --prune=now --aggressive
 git push -f
 
-git branch -D upstream/quake3e_formatted 2> /dev/null > /dev/null || echo "No Local branch, creating local branch and formatting"
+git branch -D upstream/quake3e_formatted 2>/dev/null >/dev/null || echo "No Local branch, creating local branch and formatting"
 git checkout -B upstream/quake3e_formatted
-git show ${BRANCH_WITH_CLANG_FORMAT}:.clang-format > .clang-format
+git show ${BRANCH_WITH_CLANG_FORMAT}:.clang-format >.clang-format
 # shellcheck disable=SC2038
 find code \
-     \( -name '*.h' -or -name '*.c' -or -name '*.m' \)  \
-     ! -path 'code/libcurl/*' -and \
-     ! -path 'code/libjpeg/*' -and \
-     ! -path 'code/libsdl/*' -and \
-     ! -path 'code/renderercommon/vulkan/*' \
-     | xargs clang-format -i
+    \( -name '*.h' -or -name '*.c' -or -name '*.m' \) \
+    ! -path 'code/libcurl/*' -and \
+    ! -path 'code/libjpeg/*' -and \
+    ! -path 'code/libsdl/*' -and \
+    ! -path 'code/renderercommon/vulkan/*' |
+    xargs clang-format -i
 git add code/
 rm .clang-format
 git commit -m "Format the source code with clang-format to simplify cross-branch comparisons"
