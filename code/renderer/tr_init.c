e446b3ce23db91bbe82a414d0aff569940feb3ef
Peter Donald <peter@realityforge.org>
2022-05-16 10:41:57 +1000

    Remove unused cvar

--- code/renderer/tr_init.c
+++ code/renderer/tr_init.c
@@ -46,7 +46,6 @@ cvar_t* r_railWidth;
 cvar_t* r_railCoreWidth;
 cvar_t* r_railSegmentLength;
 
-cvar_t* r_verbose;
 cvar_t* r_ignore;
 
 cvar_t* r_displayRefresh;
@@ -1015,7 +1014,6 @@ void R_Register(void)
     r_novis = ri.Cvar_Get("r_novis", "0", CVAR_CHEAT);
     r_showcluster = ri.Cvar_Get("r_showcluster", "0", CVAR_CHEAT);
     r_speeds = ri.Cvar_Get("r_speeds", "0", CVAR_CHEAT);
-    r_verbose = ri.Cvar_Get("r_verbose", "0", CVAR_CHEAT);
     r_debugSurface = ri.Cvar_Get("r_debugSurface", "0", CVAR_CHEAT);
     r_nobind = ri.Cvar_Get("r_nobind", "0", CVAR_CHEAT);
     r_showtris = ri.Cvar_Get("r_showtris", "0", CVAR_CHEAT);
