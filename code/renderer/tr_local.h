6d0d3383bac048e2f15c8733a3dbecd44a5b4938
Peter Donald <peter@realityforge.org>
2022-05-16 10:42:44 +1000

    Remove unused cvar

--- code/renderer/tr_local.h
+++ code/renderer/tr_local.h
@@ -921,7 +921,6 @@ extern cvar_t* r_railWidth;
 extern cvar_t* r_railCoreWidth;
 extern cvar_t* r_railSegmentLength;
 
-extern cvar_t* r_ignore; // used for debugging anything
 extern cvar_t* r_verbose; // used for verbose debug spew
 extern cvar_t* r_ignoreFastPath; // allows us to ignore our Tess fast paths
 
