6c9acf2c5869bc845df3e27b73d4c63e1ac77392
Peter Donald <peter@realityforge.org>
2022-05-19 19:54:26 +1000

    Format shell scripts

--- bin/setup_idSoftware_upstream.sh
+++ bin/setup_idSoftware_upstream.sh
@@ -19,12 +19,12 @@
 
 set -e
 
-git remote rm idSoftware 2> /dev/null > /dev/null || (echo "idSoftware remote not present")
+git remote rm idSoftware 2>/dev/null >/dev/null || (echo "idSoftware remote not present")
 git remote add --no-tags --track master idSoftware https://github.com/id-Software/Quake-III-Arena
 git fetch idSoftware
 
 git checkout idSoftware/master
-git branch -D upstream/idSoftware 2> /dev/null > /dev/null || (echo "idSoftware upstream branch not present")
+git branch -D upstream/idSoftware 2>/dev/null >/dev/null || (echo "idSoftware upstream branch not present")
 git checkout -B upstream/idSoftware
 git config branch.upstream/idSoftware.remote idSoftware
 git config branch.upstream/idSoftware.merge refs/heads/master
--- bin/setup_vkQuake3_upstream.sh
+++ bin/setup_vkQuake3_upstream.sh
@@ -26,12 +26,12 @@ export BRANCH_WITH_CLANG_FORMAT=origin/master
 # Fetch origin to ensure the .clang-format is present and updated
 git fetch origin
 
-git remote rm vkQuake3 2> /dev/null > /dev/null || (echo "vkQuake3 remote not present")
+git remote rm vkQuake3 2>/dev/null >/dev/null || (echo "vkQuake3 remote not present")
 git remote add --no-tags --track master vkQuake3 https://github.com/suijingfeng/vkQuake3.git
 git fetch vkQuake3
 
 git checkout vkQuake3/master
-git branch -D upstream/vkQuake3 2> /dev/null > /dev/null || (echo "vkQuake3 upstream branch not present")
+git branch -D upstream/vkQuake3 2>/dev/null >/dev/null || (echo "vkQuake3 upstream branch not present")
 git checkout -B upstream/vkQuake3
 git config branch.upstream/vkQuake3.remote vkQuake3
 git config branch.upstream/vkQuake3.merge refs/heads/master
@@ -39,27 +39,27 @@ git config branch.upstream/vkQuake3.pushRemote origin
 git gc --prune=now --aggressive
 git push
 
-git branch -D upstream/vkQuake3_formatted 2> /dev/null > /dev/null || echo "No Local branch, creating local branch and formatting"
+git branch -D upstream/vkQuake3_formatted 2>/dev/null >/dev/null || echo "No Local branch, creating local branch and formatting"
 git checkout -B upstream/vkQuake3_formatted
-git show ${BRANCH_WITH_CLANG_FORMAT}:.clang-format > .clang-format
+git show ${BRANCH_WITH_CLANG_FORMAT}:.clang-format >.clang-format
 # shellcheck disable=SC2038
 find code \
-     \( -name '*.h' -or -name '*.c' -or -name '*.m' \)  \
-     ! -path 'code/AL/*' -and \
-     ! -path 'code/curl-7.54.0/*' -and \
-     ! -path 'code/jpeg-8c/*' -and \
-     ! -path 'code/libogg-1.3.3/*' -and \
-     ! -path 'code/libvorbis-1.3.6/*' -and \
-     ! -path 'code/opus-1.2.1/*' -and \
-     ! -path 'code/opusfile-0.9/*' -and \
-     ! -path 'code/SDL2/*' -and \
-     ! -path 'code/tools/*' -and \
-     ! -path 'code/zlib/*' \
-     | xargs clang-format -i
+    \( -name '*.h' -or -name '*.c' -or -name '*.m' \) \
+    ! -path 'code/AL/*' -and \
+    ! -path 'code/curl-7.54.0/*' -and \
+    ! -path 'code/jpeg-8c/*' -and \
+    ! -path 'code/libogg-1.3.3/*' -and \
+    ! -path 'code/libvorbis-1.3.6/*' -and \
+    ! -path 'code/opus-1.2.1/*' -and \
+    ! -path 'code/opusfile-0.9/*' -and \
+    ! -path 'code/SDL2/*' -and \
+    ! -path 'code/tools/*' -and \
+    ! -path 'code/zlib/*' |
+    xargs clang-format -i
 git add code/
 rm .clang-format
 git commit -m "Format the source code with clang-format to simplify cross-branch comparisons"
-git push origin :upstream/vkQuake3_formatted 2> /dev/null > /dev/null || echo "No upstream/vkQuake3_formatted branch to delete"
+git push origin :upstream/vkQuake3_formatted 2>/dev/null >/dev/null || echo "No upstream/vkQuake3_formatted branch to delete"
 git push --set-upstream origin upstream/vkQuake3_formatted
 
 # Return to master branch on completion
