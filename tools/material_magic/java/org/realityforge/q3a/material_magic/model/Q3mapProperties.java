8582e4ac7604bd0bda81c71d468bc677e4fb0297
Peter Donald <peter@realityforge.org>
2022-05-25 11:23:38 +1000

    Start to model the q3map properties for materials explicitly

---
+++ tools/material_magic/java/org/realityforge/q3a/material_magic/model/Q3mapProperties.java
@@ -0,0 +1,76 @@
+package org.realityforge.q3a.material_magic.model;
+
+import org.realityforge.q3a.material_magic.util.MaterialOutput;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.IOException;
+
+public final class Q3mapProperties {
+    private boolean _globalTexture;
+    private boolean _forceSunLight;
+    private boolean _noVertexShadows;
+    // A positive value
+    private int _surfaceLight;
+    @Nullable
+    private String _lightImage;
+
+    public boolean globalTexture() {
+        return _globalTexture;
+    }
+
+    public void setGlobalTexture(final boolean globalTexture) {
+        _globalTexture = globalTexture;
+    }
+
+    public boolean forceSunLight() {
+        return _forceSunLight;
+    }
+
+    public void setForceSunLight(final boolean forceSunLight) {
+        _forceSunLight = forceSunLight;
+    }
+
+    public boolean noVertexShadows() {
+        return _noVertexShadows;
+    }
+
+    public void setNoVertexShadows(final boolean noVertexShadows) {
+        _noVertexShadows = noVertexShadows;
+    }
+
+    public int getSurfaceLight() {
+        return _surfaceLight;
+    }
+
+    public void setSurfaceLight(final int surfaceLight) {
+        _surfaceLight = surfaceLight;
+    }
+
+    @Nullable
+    public String getLightImage() {
+        return _lightImage;
+    }
+
+    public void setLightImage(@Nullable final String lightImage) {
+        _lightImage = lightImage;
+    }
+
+    void write(@Nonnull final MaterialOutput output) throws IOException {
+        if (0 != _surfaceLight) {
+            output.writeProperty("q3map_surfacelight", Integer.toString(_surfaceLight));
+        }
+        if (null != _lightImage) {
+            output.writeProperty("q3map_lightimage", _lightImage);
+        }
+        if (_globalTexture) {
+            output.writeProperty("q3map_lightimage");
+        }
+        if (_noVertexShadows) {
+            output.writeProperty("q3map_novertexshadows");
+        }
+        if (_forceSunLight) {
+            output.writeProperty("q3map_forcesunlight");
+        }
+    }
+}
---
+++ tools/material_magic/javatests/org/realityforge/q3a/material_magic/model/Q3mapPropertiesTest.java
@@ -0,0 +1,46 @@
+package org.realityforge.q3a.material_magic.model;
+
+import org.junit.jupiter.api.Test;
+import org.realityforge.q3a.material_magic.util.MaterialOutput;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public final class Q3mapPropertiesTest {
+    @Test
+    public void basicOperation() {
+        final Q3mapProperties properties = new Q3mapProperties();
+
+        assertFalse(properties.forceSunLight());
+        assertFalse(properties.noVertexShadows());
+        assertFalse(properties.globalTexture());
+        assertNull(properties.getLightImage());
+        assertEquals(0, properties.getSurfaceLight());
+        assertEquals("", MaterialOutput.outputAsString(properties::write));
+
+        properties.setForceSunLight(true);
+
+        assertTrue(properties.forceSunLight());
+        assertFalse(properties.noVertexShadows());
+        assertFalse(properties.globalTexture());
+        assertNull(properties.getLightImage());
+        assertEquals(0, properties.getSurfaceLight());
+        assertEquals("q3map_forcesunlight\n", MaterialOutput.outputAsString(properties::write));
+
+        properties.setNoVertexShadows(true);
+        properties.setGlobalTexture(true);
+        properties.setLightImage("images/foo");
+        properties.setSurfaceLight(500);
+
+        assertTrue(properties.forceSunLight());
+        assertTrue(properties.noVertexShadows());
+        assertTrue(properties.globalTexture());
+        assertEquals("images/foo", properties.getLightImage());
+        assertEquals(500, properties.getSurfaceLight());
+        assertEquals("q3map_surfacelight 500\n" +
+                        "q3map_lightimage images/foo\n" +
+                        "q3map_lightimage\n" +
+                        "q3map_novertexshadows\n" +
+                        "q3map_forcesunlight\n",
+                MaterialOutput.outputAsString(properties::write));
+    }
+}
