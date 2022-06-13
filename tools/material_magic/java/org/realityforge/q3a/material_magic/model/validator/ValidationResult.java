b7ef8fe9e2e4c330f5b7106802b94c6cdc266198
Peter Donald <peter@realityforge.org>
2022-06-13 22:30:53 +1000

    Add some helper methods

--- tools/material_magic/java/org/realityforge/q3a/material_magic/model/validator/ValidationResult.java
+++ tools/material_magic/java/org/realityforge/q3a/material_magic/model/validator/ValidationResult.java
@@ -2,6 +2,7 @@ package org.realityforge.q3a.material_magic.model.validator;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
 public final class ValidationResult
@@ -33,5 +34,32 @@ public final class ValidationResult
   public List<ValidationMessage> getMessages()
   {
     return _messages;
+  }
+
+  @Nonnull
+  public List<ValidationMessage> getErrorMessages()
+  {
+    return getMessages()
+      .stream()
+      .filter( m -> ValidationMessage.Type.ERROR == m.getType() )
+      .collect( Collectors.toList() );
+  }
+
+  @Nonnull
+  public List<ValidationMessage> getWarningMessages()
+  {
+    return getMessages()
+      .stream()
+      .filter( m -> ValidationMessage.Type.WARNING == m.getType() )
+      .collect( Collectors.toList() );
+  }
+
+  @Nonnull
+  public List<ValidationMessage> getInfoMessages()
+  {
+    return getMessages()
+      .stream()
+      .filter( m -> ValidationMessage.Type.INFO == m.getType() )
+      .collect( Collectors.toList() );
   }
 }
