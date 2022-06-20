62487ba00fbed4c26e0ac542e5113e565e4ea805
Peter Donald <peter@realityforge.org>
2022-06-20 12:49:49 +1000

    No need for relative paths in includes for files in same package

--- code/qcommon/net_ip.c
+++ code/qcommon/net_ip.c
@@ -20,9 +20,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-#include "../qcommon/q_shared.h"
-#include "../qcommon/qcommon.h"
-#include "../qcommon/qengine.h"
+#include "q_shared.h"
+#include "qcommon.h"
+#include "qengine.h"
 
 #ifdef _WIN32
 #include <winsock2.h>
