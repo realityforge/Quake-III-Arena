59b20b1e05759ae3f75da2eec52156f08f993de6
Peter Donald <peter@realityforge.org>
2022-06-20 13:14:57 +1000

    Include header from local package

--- code/botlib/l_script.c
+++ code/botlib/l_script.c
@@ -30,7 +30,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifdef BOTLIB
 // include files for usage in the bot library
 #include "../qcommon/q_shared.h"
-#include "../game/botlib.h"
+#include "botlib.h"
 #include "be_interface.h"
 #include "l_script.h"
 #include "l_memory.h"
