8741479ac7a4d75360271dd69ea9fa598c5c82dc
Peter Donald <peter@realityforge.org>
2022-06-29 19:52:52 +1000

    Remove unnecessary #include

--- code/qcommon/cvar_common.h
+++ code/qcommon/cvar_common.h
@@ -22,8 +22,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef CVAR_H
 #define CVAR_H
 
-#include <stdbool.h>
-
 /*
 ==========================================================
 
