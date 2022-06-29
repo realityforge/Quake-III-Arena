c2c80e8b17c68ce186a9be45dc3cad73288daedf
Peter Donald <peter@realityforge.org>
2022-06-29 20:01:24 +1000

    Add some missing brackets for clarity

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -1149,8 +1149,9 @@ long FS_FOpenFileRead(const char* filename, fileHandle_t* file, bool uniqueFILE)
     long len;
     bool isLocalConfig;
 
-    if (!fs_searchpaths)
+    if (!fs_searchpaths) {
         Com_Error(ERR_FATAL, "Filesystem call made without initialization");
+    }
 
     isLocalConfig = !strcmp(filename, "autoexec.cfg") || !strcmp(filename, Q3CONFIG_CFG);
     for (search = fs_searchpaths; search; search = search->next) {
