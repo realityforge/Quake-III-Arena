d458a30e31ac325eacf7833ac9bce44b5685b8a4
Peter Donald <peter@realityforge.org>
2022-07-12 23:20:57 +1000

    Remove commented code

--- code/botlib/be_aas_sample.c
+++ code/botlib/be_aas_sample.c
@@ -858,36 +858,7 @@ aas_face_t* AAS_TraceEndFace(aas_trace_t* trace)
         face = &aasworld.faces[abs(facenum)];
         // if the face is in the same plane as the trace end point
         if ((face->planenum & ~1) == (trace->planenum & ~1)) {
-            // firstface is used for optimization, if there's only one
-            // face in the plane then it has to be the good one
-            // if there are more faces in the same plane then always
-            // check the one with the fewest edges first
-            /*			if (firstface)
-                                    {
-                                            if (firstface->numedges < face->numedges)
-                                            {
-                                                    if (AAS_InsideFace(firstface,
-                                                            aasworld.planes[face->planenum].normal, trace->endpos))
-                                                    {
-                                                            return firstface;
-                                                    }
-                                                    firstface = face;
-                                            }
-                                            else
-                                            {
-                                                    if (AAS_InsideFace(face,
-                                                            aasworld.planes[face->planenum].normal, trace->endpos))
-                                                    {
-                                                            return face;
-                                                    }
-                                            }
-                                    }
-                                    else
-                                    {
-                                            firstface = face;
-                                    }*/
-            if (AAS_InsideFace(face,
-                               aasworld.planes[face->planenum].normal, trace->endpos, 0.01f))
+            if (AAS_InsideFace(face, aasworld.planes[face->planenum].normal, trace->endpos, 0.01f))
                 return face;
         }
     }
