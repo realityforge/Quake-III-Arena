bca5efe8089d49b201a5a371d471374f54f8fb0f
Peter Donald <peter@realityforge.org>
2022-07-12 22:57:55 +1000

    Remove unnecessary #include

--- code/botlib/be_aas_bspq3.c
+++ code/botlib/be_aas_bspq3.c
@@ -32,7 +32,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "aasfile.h"
 #include "botlib.h"
 #include "be_aas.h"
-#include "be_aas_funcs.h"
 #include "be_aas_def.h"
 #include "be_aas_bsp.h"
 
