bc388a86a9740b76e4bd1e1abd84e297b12d4fb2
Peter Donald <peter@realityforge.org>
2022-07-13 12:13:21 +1000

    managment -> management

--- code/ui/ui_shared.c
+++ code/ui/ui_shared.c
@@ -19,7 +19,7 @@ along with Quake III Arena source code; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
-// string allocation/managment
+// string allocation/management
 
 #include "ui_shared.h"
 #include "ui/menudef.h"
