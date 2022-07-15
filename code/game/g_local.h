65c739ddebd69f2d57c144502b96630a30329b21
Peter Donald <peter@realityforge.org>
2022-07-15 21:06:32 +1000

    Mark function as non-returning

--- code/game/g_local.h
+++ code/game/g_local.h
@@ -606,7 +606,7 @@ void G_RunThink(gentity_t* ent);
 void QDECL G_LogPrintf(const char* fmt, ...);
 void SendScoreboardMessageToAllClients(void);
 void QDECL G_Printf(const char* fmt, ...);
-void QDECL G_Error(const char* fmt, ...);
+void QDECL G_Error(const char* fmt, ...) NORETURN;
 
 //
 // g_client.c
