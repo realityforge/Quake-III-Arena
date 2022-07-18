5890213d75169d4c3fdf31cd94f77bbc9a0e1039
Peter Donald <peter@realityforge.org>
2022-07-18 21:19:45 +1000

    Remove empty comment

--- code/botlib/be_aas_reach.h
+++ code/botlib/be_aas_reach.h
@@ -25,7 +25,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 void AAS_InitReachability(void);
 // continue calculating the reachabilities
 int AAS_ContinueInitReachability(void);
-//
 int AAS_BestReachableLinkArea(aas_link_t* areas);
 #endif // AASINTERN
 
