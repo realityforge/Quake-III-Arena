3b41ab53d44dce53d44f59c36ce2395b8b3a5678
Peter Donald <peter@realityforge.org>
2022-07-20 22:35:02 +1000

    Remove errors if certain files compiled as part of Q3VM component as we are moving towards a wasm environment

--- code/ui/ui_syscalls.c
+++ code/ui/ui_syscalls.c
@@ -23,12 +23,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "ui_local.h"
 #include "plugin.h"
 
-// this file is only included when building a dll
-// syscalls.asm is included instead when building a qvm
-#ifdef Q3_VM
-#error "Do not use in VM build"
-#endif
-
 static vmDllSystemCall syscall = (vmDllSystemCall)-1;
 
 EXPORT void dllEntry(vmDllSystemCall syscallptr)
