171a5605f3194d9c1f8e1a0305e8a3dad7f31f6c
Peter Donald <peter@realityforge.org>
2022-07-22 18:11:41 +1000

    Move declaration of variables to first usage and make const where possible

--- code/renderer/tr_noise.c
+++ code/renderer/tr_noise.c
@@ -52,25 +52,22 @@ void R_NoiseInit(void)
     }
 }
 
-float R_NoiseGet4f(float x, float y, float z, float t)
+float R_NoiseGet4f(const float x, const float y, const float z, const float t)
 {
-    int i;
-    int ix, iy, iz, it;
-    float fx, fy, fz, ft;
-    float front[4];
-    float back[4];
-    float fvalue, bvalue, value[2], finalvalue;
+    const int ix = (int)floor(x);
+    const float fx = x - ix;
+    const int iy = (int)floor(y);
+    const float fy = y - iy;
+    const int iz = (int)floor(z);
+    const float fz = z - iz;
+    const int it = (int)floor(t);
+    const float ft = t - it;
 
-    ix = (int)floor(x);
-    fx = x - ix;
-    iy = (int)floor(y);
-    fy = y - iy;
-    iz = (int)floor(z);
-    fz = z - iz;
-    it = (int)floor(t);
-    ft = t - it;
+    float value[2];
+    for (int i = 0; i < 2; i++) {
+        float front[4];
+        float back[4];
 
-    for (i = 0; i < 2; i++) {
         front[0] = GetNoiseValue(ix, iy, iz, it + i);
         front[1] = GetNoiseValue(ix + 1, iy, iz, it + i);
         front[2] = GetNoiseValue(ix, iy + 1, iz, it + i);
@@ -81,13 +78,11 @@ float R_NoiseGet4f(float x, float y, float z, float t)
         back[2] = GetNoiseValue(ix, iy + 1, iz + 1, it + i);
         back[3] = GetNoiseValue(ix + 1, iy + 1, iz + 1, it + i);
 
-        fvalue = LERP(LERP(front[0], front[1], fx), LERP(front[2], front[3], fx), fy);
-        bvalue = LERP(LERP(back[0], back[1], fx), LERP(back[2], back[3], fx), fy);
+        const float fvalue = LERP(LERP(front[0], front[1], fx), LERP(front[2], front[3], fx), fy);
+        const float bvalue = LERP(LERP(back[0], back[1], fx), LERP(back[2], back[3], fx), fy);
 
         value[i] = LERP(fvalue, bvalue, fz);
     }
 
-    finalvalue = LERP(value[0], value[1], ft);
-
-    return finalvalue;
+    return LERP(value[0], value[1], ft);
 }
