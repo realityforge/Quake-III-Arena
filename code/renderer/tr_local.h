b1b0f5d8526c38a91ac50bb87d2a5c3a573129f8
Peter Donald <peter@realityforge.org>
2022-07-23 10:50:09 +1000

    Remove unused function

--- code/renderer/tr_local.h
+++ code/renderer/tr_local.h
@@ -1244,7 +1244,6 @@ void RB_ProjectionShadowDeform(void);
 
 void R_BuildCloudData(shaderCommands_t* shader);
 void R_InitSkyTexCoords(float cloudLayerHeight);
-void RB_DrawSun(void);
 void RB_ClipSkyPolygons(shaderCommands_t* shader);
 
 #define PATCH_STITCHING
