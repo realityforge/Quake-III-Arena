179b03028b7ffdc56e29053c02a0de6fbaf4ccd5
Peter Donald <peter@realityforge.org>
2022-08-02 20:33:21 +1000

    Revert addition of sdl_local library

--- code/sdl/sdl_input.c
+++ code/sdl/sdl_input.c
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-#include "sdl_local.h"
+#include "SDL2/SDL.h"
 #include "lang_util.h"
 
 #include <stdio.h>
