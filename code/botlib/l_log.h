fd9116d7e2058a15d0f9f4ebf26b4b7a6701f7f8
Peter Donald <peter@realityforge.org>
2022-08-03 10:46:28 +1000

    Add missing include

--- code/botlib/l_log.h
+++ code/botlib/l_log.h
@@ -20,6 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
+#include "attributes.h"
 #include "q_shared.h"
 
 // open a log file
