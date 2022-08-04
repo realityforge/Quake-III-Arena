2c7b4f233d969124dbd006e01d0f2e31fca70ead
Peter Donald <peter@realityforge.org>
2022-08-04 10:55:47 +1000

    No need to shorten string by a single character

--- code/botlib/l_struct.c
+++ code/botlib/l_struct.c
@@ -173,7 +173,7 @@ static int ReadString(source_t* source, void* p)
     // remove the double quotes
     StripDoubleQuotes(token.string);
     // copy the string
-    strncpyz((char*)p, token.string, MAX_STRINGFIELD - 1);
+    strncpyz((char*)p, token.string, MAX_STRINGFIELD);
     return 1;
 }
 int ReadStructure(source_t* source, structdef_t* def, char* structure)
