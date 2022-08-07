61c0193a6e8fa5f4db4d99abfc8bef471927eb38
Peter Donald <peter@realityforge.org>
2022-08-07 22:33:24 +1000

    Group includes

--- code/cgame/cg_newdraw.c
+++ code/cgame/cg_newdraw.c
@@ -26,8 +26,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "cg_local.h"
 #include "../ui/ui_shared.h"
-#include "voicechat.h"
 #include "ui/menudef.h"
+#include "voicechat.h"
 
 extern displayContextDef_t cgDC;
 
