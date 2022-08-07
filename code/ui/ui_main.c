a67f98b9d42ab8bc511e3ae51627c8dfaa2d48ac
Peter Donald <peter@realityforge.org>
2022-08-07 22:07:52 +1000

    Remove commented code

--- code/ui/ui_main.c
+++ code/ui/ui_main.c
@@ -354,8 +354,6 @@ static void Text_Paint(float x, float y, float scale, vec4_t color, const char* 
         count = 0;
         while (s && *s && count < len) {
             glyph = &font->glyphs[*s & 255];
-            // int yadj = Assets.textFont.glyphs[text[i]].bottom + Assets.textFont.glyphs[text[i]].top;
-            // float yadj = scale * (Assets.textFont.glyphs[text[i]].imageHeight - Assets.textFont.glyphs[text[i]].height);
             if (Q_IsColorString(s)) {
                 memcpy(newColor, g_color_table[ColorIndex(*(s + 1))], sizeof(newColor));
                 newColor[3] = color[3];
@@ -425,8 +423,6 @@ static void Text_PaintWithCursor(float x, float y, float scale, vec4_t color, co
         glyph2 = &font->glyphs[cursor & 255];
         while (s && *s && count < len) {
             glyph = &font->glyphs[*s & 255];
-            // int yadj = Assets.textFont.glyphs[text[i]].bottom + Assets.textFont.glyphs[text[i]].top;
-            // float yadj = scale * (Assets.textFont.glyphs[text[i]].imageHeight - Assets.textFont.glyphs[text[i]].height);
             if (Q_IsColorString(s)) {
                 memcpy(newColor, g_color_table[ColorIndex(*(s + 1))], sizeof(newColor));
                 newColor[3] = color[3];
