59a5201621c982a87e7b55b97dc2231b01eadb6f
Peter Donald <peter@realityforge.org>
2022-08-07 20:24:56 +1000

    Remove commented code

--- code/qcommon/q_shared.c
+++ code/qcommon/q_shared.c
@@ -140,21 +140,6 @@ void COM_DefaultExtension(char* path, int maxSize, const char* extension)
 
 ============================================================================
 */
-/*
-// can't just use function pointers, or dll linkage can
-// mess up when qcommon is included in multiple places
-static short	(*_BigShort) (short l);
-static short	(*_LittleShort) (short l);
-static int		(*_LittleLong) (int l);
-static qint64	(*_LittleLong64) (qint64 l);
-static float	(*_LittleFloat) (const float *l);
-
-short	BigShort(short l){return _BigShort(l);}
-short	LittleShort(short l) {return _LittleShort(l);}
-int		LittleLong (int l) {return _LittleLong(l);}
-qint64 	LittleLong64 (qint64 l) {return _LittleLong64(l);}
-float	LittleFloat (const float *l) {return _LittleFloat(l);}
-*/
 
 short ShortSwap(short l)
 {
