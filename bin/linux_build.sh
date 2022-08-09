19bb59777c28f2938da8010a2d5ee21d44e84047
Peter Donald <peter@realityforge.org>
2022-08-09 19:45:25 +1000

    Backport changes from main branch

--- bin/linux_build.sh
+++ bin/linux_build.sh
@@ -14,30 +14,33 @@
 
 set -e
 
-export WORKSPACE_BASE=`pwd`
+export WORKSPACE_BASE=$(pwd)
+export IMAGE_NAME="q3a_builder-`git branch --show-current | tr -d '\n'`"
 
-export IMAGE_PRESENT=`docker images | grep q3a_build`
+export IMAGE_PRESENT=$(docker images | grep q3a_build)
 if [ "" = "${IMAGE_PRESENT}" ] || [ "${FORCE_BUILD}" == "true" ]; then
-  echo "Creating Build Image"
-  docker build -t q3a_build:latest ${WORKSPACE_BASE}/bin/docker/linux
-  docker rm --force q3a_builder 2> /dev/null > /dev/null
+    echo "Creating Build Image"
+    docker build -t q3a_build:latest ${WORKSPACE_BASE}/bin/docker/linux
+    docker rm --force "${IMAGE_NAME}" 2>/dev/null >/dev/null
 fi
 
-export CONTAINER_PRESENT=`docker ps -a | grep q3a_builder`
+export CONTAINER_PRESENT=$(docker ps -a | grep "${IMAGE_NAME}")
 if [ "" = "${CONTAINER_PRESENT}" ]; then
-  echo "Creating Build Container"
-  docker create --tty \
-                -v ${WORKSPACE_BASE}/:/home/q3a_builder/source \
-                -v ${HOME}/.cache/bazel-repo:/home/q3a_builder/.cache/bazel-repo \
-                -v ${HOME}/.cache/bazel-linux-disk:/home/q3a_builder/.cache/bazel-disk \
-                --name q3a_builder \
-                q3a_build:latest
+    echo "Creating Build Container - ${IMAGE_NAME}"
+    docker create --tty \
+        --mount type=bind,source="${WORKSPACE_BASE}/",target=/home/q3a_builder/source \
+        --mount type=bind,source="${HOME}/.cache/bazel-repo",target=/home/q3a_builder/.cache/bazel-repo \
+        --mount type=bind,source="${HOME}/.cache/bazel-linux-disk",target=/home/q3a_builder/.cache/bazel-disk \
+        --name "${IMAGE_NAME}" \
+        q3a_build:latest
 fi
 
-export CONTAINER_RUNNING=`docker ps | grep q3a_builder`
+export CONTAINER_RUNNING=$(docker ps | grep "${IMAGE_NAME}")
 if [ "" = "${CONTAINER_RUNNING}" ]; then
-  echo "Starting Build Container"
-  docker start  q3a_builder
+    echo "Starting Build Container - ${IMAGE_NAME}"
+    docker start "${IMAGE_NAME}"
 fi
 
-docker exec --interactive --tty q3a_builder ./bazelw $@
+docker exec --interactive --tty "${IMAGE_NAME}" ./bazelw $@
+
+docker stop "${IMAGE_NAME}"
