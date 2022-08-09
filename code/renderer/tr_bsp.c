f4a98b35d73ee25f8a91c7436377eab7f9da3bfe
Peter Donald <peter@realityforge.org>
2022-08-10 09:34:23 +1000

    Avoid casting of double literal to a float

--- code/renderer/tr_bsp.c
+++ code/renderer/tr_bsp.c
@@ -1966,7 +1966,8 @@ static void R_LoadFogs(lump_t* l, lump_t* brushesLump, lump_t* sidesLump)
 
         out->colorInt = ColorBytes4(shader->fogParms.color[0],
                                     shader->fogParms.color[1],
-                                    shader->fogParms.color[2], 1.0);
+                                    shader->fogParms.color[2],
+                                    1.0F);
 
         d = shader->fogParms.depthForOpaque < 1 ? 1 : shader->fogParms.depthForOpaque;
         out->tcScale = 1.0f / (d * 8);
