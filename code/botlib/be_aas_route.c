82848120dd837c20d63a6f090708f391725b918b
Peter Donald <peter@realityforge.org>
2022-08-12 19:58:01 +1000

    Avoid constant conditions due to variables usage

--- code/botlib/be_aas_route.c
+++ code/botlib/be_aas_route.c
@@ -106,9 +106,9 @@ static inline int AAS_TravelFlagForType_inline(int traveltype)
     int tfl;
 
     tfl = 0;
-    if (tfl & TRAVELFLAG_NOTTEAM1)
+    if (traveltype & TRAVELFLAG_NOTTEAM1)
         tfl |= TFL_NOTTEAM1;
-    if (tfl & TRAVELFLAG_NOTTEAM2)
+    if (traveltype & TRAVELFLAG_NOTTEAM2)
         tfl |= TFL_NOTTEAM2;
     traveltype &= TRAVELTYPE_MASK;
     if (traveltype < 0 || traveltype >= MAX_TRAVELTYPES)
