9cede2912b3758566f9029b0019f699010546bab
Peter Donald <peter@realityforge.org>
2022-08-13 20:55:51 +1000

    Code cleanup

--- code/qcommon/cvar_engine.h
+++ code/qcommon/cvar_engine.h
@@ -86,7 +86,7 @@ int Cvar_VariableIntegerValue(const char* var_name);
 // returns 0 if not defined or non numeric
 
 char* Cvar_VariableString(const char* var_name);
-void Cvar_VariableStringBuffer(const char* var_name, char* buffer, int bufsize);
+void Cvar_VariableStringBuffer(const char* var_name, char* buffer, const size_t bufsize);
 // returns an empty string if not defined
 
 int Cvar_Flags(const char* var_name);
