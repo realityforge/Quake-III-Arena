8913e613844a0ddfef36cdf49d3bbdaecd470979
Peter Donald <peter@realityforge.org>
2022-08-18 23:14:19 +1000

    Remove stray wolf3d code

--- code/cgame/cg_particles.c
+++ code/cgame/cg_particles.c
@@ -87,20 +87,6 @@ typedef enum {
 #define MAX_SHADER_ANIMS 32
 #define MAX_SHADER_ANIM_FRAMES 64
 
-#ifndef WOLF_PARTICLES
-static char* shaderAnimNames[MAX_SHADER_ANIMS] = {
-    "explode1",
-    NULL
-};
-static qhandle_t shaderAnims[MAX_SHADER_ANIMS][MAX_SHADER_ANIM_FRAMES];
-static int shaderAnimCounts[MAX_SHADER_ANIMS] = {
-    23
-};
-static float shaderAnimSTRatio[MAX_SHADER_ANIMS] = {
-    1.0f
-};
-// done.
-#else
 static char* shaderAnimNames[MAX_SHADER_ANIMS] = {
     "explode1",
     "blacksmokeanim",
@@ -127,15 +113,10 @@ static float shaderAnimSTRatio[MAX_SHADER_ANIMS] = {
     1.0f,
     1.0f,
 };
-#endif
 
 #define PARTICLE_GRAVITY 40
 
-#ifdef WOLF_PARTICLES
-#define MAX_PARTICLES 1024 * 8
-#else
 #define MAX_PARTICLES 1024
-#endif
 
 cparticle_t *active_particles, *free_particles;
 cparticle_t particles[MAX_PARTICLES];
@@ -312,11 +293,7 @@ static void CG_AddParticleToScene(cparticle_t* p, vec3_t org, float alpha)
         vec3_t rr, ru;
         vec3_t rotate_ang;
 
-#ifdef WOLF_PARTICLES
-        VectorSet(color, 1.0, 1.0, 1.0);
-#else
         VectorSet(color, 1.0, 1.0, 0.5);
-#endif
         time = cg.time - p->time;
         time2 = p->endtime - p->time;
         ratio = time / time2;
@@ -904,11 +881,7 @@ void CG_ParticleExplosion(char* animStr, vec3_t origin, vec3_t vel, int duration
     p->next = active_particles;
     active_particles = p;
     p->time = cg.time;
-#ifdef WOLF_PARTICLES
-    p->alpha = 1.0;
-#else
     p->alpha = 0.5;
-#endif
     p->alphavel = 0;
 
     if (duration < 0) {
