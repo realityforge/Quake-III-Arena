c12b605e1ad526a3c37bef1e866551311f5bf5e1
Peter Donald <peter@realityforge.org>
2022-08-18 22:54:54 +1000

    Backport changes from master branch

--- code/cgame/cg_info.c
+++ code/cgame/cg_info.c
@@ -33,21 +33,15 @@ static qhandle_t loadingItemIcons[MAX_LOADING_ITEM_ICONS];
 
 static void CG_DrawLoadingIcons()
 {
-    int n;
-    int x, y;
-
-    for (n = 0; n < loadingPlayerIconCount; n++) {
-        x = 16 + n * 78;
-        y = 324 - 40;
+    for (int n = 0; n < loadingPlayerIconCount; n++) {
+        const int x = 16 + n * 78;
+        const int y = 324 - 40;
         CG_DrawPic(x, y, 64, 64, loadingPlayerIcons[n]);
     }
 
-    for (n = 0; n < loadingItemIconCount; n++) {
-        y = 400 - 40;
-        if (n >= 13) {
-            y += 40;
-        }
-        x = 16 + n % 13 * 48;
+    for (int n = 0; n < loadingItemIconCount; n++) {
+        const int y = 400 - 40 + (n >= 13 ? 40 : 0);
+        const int x = 16 + n % 13 * 48;
         CG_DrawPic(x, y, 32, 32, loadingItemIcons[n]);
     }
 }
@@ -55,15 +49,12 @@ static void CG_DrawLoadingIcons()
 void CG_LoadingString(const char* s)
 {
     strncpyz(cg.infoScreenText, s, sizeof(cg.infoScreenText));
-
     trap_UpdateScreen();
 }
 
-void CG_LoadingItem(int itemNum)
+void CG_LoadingItem(const int itemNum)
 {
-    gitem_t* item;
-
-    item = &bg_itemlist[itemNum];
+    const gitem_t* item = &bg_itemlist[itemNum];
 
     if (item->icon && loadingItemIconCount < MAX_LOADING_ITEM_ICONS) {
         loadingItemIcons[loadingItemIconCount++] = trap_R_RegisterShaderNoMip(item->icon);
@@ -74,18 +65,16 @@ void CG_LoadingItem(int itemNum)
 
 void CG_LoadingClient(int clientNum)
 {
-    const char* info;
-    char* skin;
     char personality[MAX_QPATH];
     char model[MAX_QPATH];
     char iconName[MAX_QPATH];
 
-    info = CG_ConfigString(CS_PLAYERS + clientNum);
+    const char* info = CG_ConfigString(CS_PLAYERS + clientNum);
 
     if (loadingPlayerIconCount < MAX_LOADING_PLAYER_ICONS) {
         strncpyz(model, Info_ValueForKey(info, "model"), sizeof(model));
-        skin = strrchr(model, '/');
-        if (skin) {
+        char* skin = strrchr(model, '/');
+        if (NULL != skin) {
             *skin++ = '\0';
         } else {
             skin = "default";
@@ -110,7 +99,7 @@ void CG_LoadingClient(int clientNum)
     strncpyz(personality, Info_ValueForKey(info, "n"), sizeof(personality));
     Q_CleanStr(personality);
 
-    if (cgs.gametype == GT_SINGLE_PLAYER) {
+    if (GT_SINGLE_PLAYER == cgs.gametype) {
         trap_S_RegisterSound(va("sound/player/announce/%s.wav", personality), true);
     }
 
@@ -126,20 +115,11 @@ Draw all the status / pacifier stuff during level loading
 */
 void CG_DrawInformation()
 {
-    const char* s;
-    const char* info;
-    const char* sysInfo;
-    int y;
-    int value;
-    qhandle_t levelshot;
-    qhandle_t detail;
-    char buf[1024];
+    const char* info = CG_ConfigString(CS_SERVERINFO);
+    const char* sysInfo = CG_ConfigString(CS_SYSTEMINFO);
 
-    info = CG_ConfigString(CS_SERVERINFO);
-    sysInfo = CG_ConfigString(CS_SYSTEMINFO);
-
-    s = Info_ValueForKey(info, "mapname");
-    levelshot = trap_R_RegisterShaderNoMip(va("levelshots/%s", s));
+    const char* mapname = Info_ValueForKey(info, "mapname");
+    qhandle_t levelshot = trap_R_RegisterShaderNoMip(va("levelshots/%s", mapname));
     if (!levelshot) {
         levelshot = trap_R_RegisterShaderNoMip("menu/art/unknownmap");
     }
@@ -147,7 +127,7 @@ void CG_DrawInformation()
     CG_DrawPic(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, levelshot);
 
     // blend a detail texture over it
-    detail = trap_R_RegisterShader("levelShotDetail");
+    const qhandle_t detail = trap_R_RegisterShader("levelShotDetail");
     trap_R_DrawStretchPic(0, 0, cgs.glconfig.vidWidth, cgs.glconfig.vidHeight, 0, 0, 2.5, 2, detail);
 
     // draw the icons of things as they are loaded
@@ -165,9 +145,11 @@ void CG_DrawInformation()
 
     // draw info string information
 
-    y = 180 - 32;
+    int y = 180 - 32;
 
     // don't print server lines if playing a local game
+    char buf[1024];
+
     trap_Cvar_VariableStringBuffer("sv_running", buf, sizeof(buf));
     if (!atoi(buf)) {
         // server hostname
@@ -178,18 +160,16 @@ void CG_DrawInformation()
         y += PROP_HEIGHT;
 
         // pure server
-        s = Info_ValueForKey(sysInfo, "sv_pure");
-        if (s[0] == '1') {
-            UI_DrawProportionalString(320, y, "Pure Server",
-                                      UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
+        const char* sv_pure = Info_ValueForKey(sysInfo, "sv_pure");
+        if (sv_pure[0] == '1') {
+            UI_DrawProportionalString(320, y, "Pure Server", UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
             y += PROP_HEIGHT;
         }
 
         // server-specific message of the day
-        s = CG_ConfigString(CS_MOTD);
-        if (s[0]) {
-            UI_DrawProportionalString(320, y, s,
-                                      UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
+        const char* motd = CG_ConfigString(CS_MOTD);
+        if (motd[0]) {
+            UI_DrawProportionalString(320, y, motd, UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
             y += PROP_HEIGHT;
         }
 
@@ -198,78 +178,73 @@ void CG_DrawInformation()
     }
 
     // map-specific message (long map name)
-    s = CG_ConfigString(CS_MESSAGE);
-    if (s[0]) {
-        UI_DrawProportionalString(320, y, s,
-                                  UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
+    const char* message = CG_ConfigString(CS_MESSAGE);
+    if (message[0]) {
+        UI_DrawProportionalString(320, y, message, UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
         y += PROP_HEIGHT;
     }
 
     // cheats warning
-    s = Info_ValueForKey(sysInfo, "sv_cheats");
-    if (s[0] == '1') {
-        UI_DrawProportionalString(320, y, "CHEATS ARE ENABLED",
-                                  UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
+    const char* sv_cheats = Info_ValueForKey(sysInfo, "sv_cheats");
+    if (sv_cheats[0] == '1') {
+        UI_DrawProportionalString(320, y, "CHEATS ARE ENABLED", UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
         y += PROP_HEIGHT;
     }
 
+    const char* game_type;
     // game type
     switch (cgs.gametype) {
     case GT_FFA:
-        s = "Free For All";
+        game_type = "Free For All";
         break;
     case GT_SINGLE_PLAYER:
-        s = "Single Player";
+        game_type = "Single Player";
         break;
     case GT_TOURNAMENT:
-        s = "Tournament";
+        game_type = "Tournament";
         break;
     case GT_TEAM:
-        s = "Team Deathmatch";
+        game_type = "Team Deathmatch";
         break;
     case GT_CTF:
-        s = "Capture The Flag";
+        game_type = "Capture The Flag";
         break;
 #ifdef MISSIONPACK
     case GT_1FCTF:
-        s = "One Flag CTF";
+        game_type = "One Flag CTF";
         break;
     case GT_OBELISK:
-        s = "Overload";
+        game_type = "Overload";
         break;
     case GT_HARVESTER:
-        s = "Harvester";
+        game_type = "Harvester";
         break;
 #endif
     default:
-        s = "Unknown Gametype";
+        game_type = "Unknown Gametype";
         break;
     }
-    UI_DrawProportionalString(320, y, s,
-                              UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
+    UI_DrawProportionalString(320, y, game_type, UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
     y += PROP_HEIGHT;
 
-    value = atoi(Info_ValueForKey(info, "timelimit"));
-    if (value) {
-        UI_DrawProportionalString(320, y, va("timelimit %i", value),
-                                  UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
+    const int timelimit = atoi(Info_ValueForKey(info, "timelimit"));
+    if (0 != timelimit) {
+        UI_DrawProportionalString(320, y, va("timelimit %i", timelimit), UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
         y += PROP_HEIGHT;
     }
 
     if (cgs.gametype < GT_CTF) {
-        value = atoi(Info_ValueForKey(info, "fraglimit"));
-        if (value) {
-            UI_DrawProportionalString(320, y, va("fraglimit %i", value),
-                                      UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
+        const int fraglimit = atoi(Info_ValueForKey(info, "fraglimit"));
+        if (0 != fraglimit) {
+            UI_DrawProportionalString(320, y, va("fraglimit %i", fraglimit), UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
             y += PROP_HEIGHT;
         }
     }
 
     if (cgs.gametype >= GT_CTF) {
-        value = atoi(Info_ValueForKey(info, "capturelimit"));
-        if (value) {
-            UI_DrawProportionalString(320, y, va("capturelimit %i", value),
-                                      UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
+        const int capturelimit = atoi(Info_ValueForKey(info, "capturelimit"));
+        if (0 != capturelimit) {
+            UI_DrawProportionalString(320, y, va("capturelimit %i", capturelimit), UI_CENTER | UI_SMALLFONT | UI_DROPSHADOW, colorWhite);
         }
     }
 }
