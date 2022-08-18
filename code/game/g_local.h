385a44ea0027109c793777b174f345ea156eefb1
Peter Donald <peter@realityforge.org>
2022-08-18 17:32:10 +1000

    Remove unused define

--- code/game/g_local.h
+++ code/game/g_local.h
@@ -34,8 +34,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #define BODY_QUEUE_SIZE 8
 
-#define INFINITE 1000000
-
 #define FRAMETIME 100 // msec
 #define CARNAGE_REWARD_TIME 3000
 #define REWARD_SPRITE_TIME 2000
