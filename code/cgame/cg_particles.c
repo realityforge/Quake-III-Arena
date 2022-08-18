0cb362d36774cd33e751e39fb6007e53f5aeefe0
Peter Donald <peter@realityforge.org>
2022-08-18 21:49:18 +1000

    Remove commented define

--- code/cgame/cg_particles.c
+++ code/cgame/cg_particles.c
@@ -24,8 +24,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "cg_local.h"
 
-//#define WOLF_PARTICLES
-
 #define BLOODRED 2
 #define EMISIVEFADE 3
 #define GREY75 4
