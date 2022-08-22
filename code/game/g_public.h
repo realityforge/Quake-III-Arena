c4321af526cbc0931319d6e2c2e7ff106ff440ac
Peter Donald <peter@realityforge.org>
2022-08-23 09:52:09 +1000

    Revert comments

--- code/game/g_public.h
+++ code/game/g_public.h
@@ -77,8 +77,8 @@ typedef struct {
     // when a trace call is made and passEntityNum != ENTITYNUM_NONE,
     // an ent will be excluded from testing if:
     // ent->s.number == passEntityNum	(don't interact with self)
-    // ent->r.ownerNum == passEntityNum	(don't interact with your own missiles)
-    // entity[ent->r.ownerNum].r.ownerNum == passEntityNum	(don't interact with other missiles from owner)
+    // ent->s.ownerNum = passEntityNum	(don't interact with your own missiles)
+    // entity[ent->s.ownerNum].ownerNum = passEntityNum	(don't interact with other missiles from owner)
     int ownerNum;
 } entityShared_t;
 
