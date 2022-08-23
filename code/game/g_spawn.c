a5958b9be354c121bbc33dd90f5b218c2ce7e08c
Peter Donald <peter@realityforge.org>
2022-08-23 10:20:39 +1000

    Remove commented code

--- code/game/g_spawn.c
+++ code/game/g_spawn.c
@@ -29,7 +29,6 @@ bool G_SpawnString(const char* key, const char* defaultString, char** out)
 
     if (!level.spawning) {
         *out = (char*)defaultString;
-        //		G_Error( "G_SpawnString() called while not spawning" );
     }
 
     for (i = 0; i < level.numSpawnVars; i++) {
