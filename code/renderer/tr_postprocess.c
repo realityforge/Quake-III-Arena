2efe9c4240ed0abc551c02a947863217820168d3
Peter Donald <peter@realityforge.org>
2022-08-24 10:19:18 +1000

    Remove unused code

--- code/renderer/tr_postprocess.c
+++ code/renderer/tr_postprocess.c
@@ -121,21 +121,12 @@ void RB_BokehBlur(FBO_t* src, ivec4_t srcBox, FBO_t* dst, ivec4_t dstBox, float 
             FBO_FastBlit(src, srcBox, tr.quarterFbo[0], quarterBox, GL_COLOR_BUFFER_BIT, GL_LINEAR);
         }
 
-#ifndef HQ_BLUR
-        if (blur > 1.0f) {
-            // create a 1/16th texture
-            // FBO_Blit(tr.quarterFbo[0], NULL, NULL, tr.textureScratchFbo[0], NULL, NULL, NULL, 0);
-            FBO_FastBlit(tr.quarterFbo[0], NULL, tr.textureScratchFbo[0], NULL, GL_COLOR_BUFFER_BIT, GL_LINEAR);
-        }
-#endif
-
         if (blur > 0.0f && blur <= 1.0f) {
             // Crossfade original with quarter texture
             VectorSet4(color, 1, 1, 1, blur);
 
             FBO_Blit(tr.quarterFbo[0], NULL, NULL, dst, dstBox, NULL, color, GLS_SRCBLEND_SRC_ALPHA | GLS_DSTBLEND_ONE_MINUS_SRC_ALPHA);
         }
-#ifndef HQ_BLUR
         // ok blur, but can see some pixelization
         else if (blur > 1.0f && blur <= 2.0f) {
             // crossfade quarter texture with 1/16th texture
@@ -167,35 +158,5 @@ void RB_BokehBlur(FBO_t* src, ivec4_t srcBox, FBO_t* dst, ivec4_t dstBox, float 
 
             FBO_Blit(tr.textureScratchFbo[1], NULL, NULL, dst, dstBox, NULL, NULL, 0);
         }
-#else // higher quality blur, but slower
-        else if (blur > 1.0f) {
-            // blur quarter texture then replace
-            int i;
-
-            src = tr.quarterFbo[0];
-            dst = tr.quarterFbo[1];
-
-            VectorSet4(color, 0.5f, 0.5f, 0.5f, 1);
-
-            for (i = 0; i < 2; i++) {
-                vec2_t blurTexScale;
-                float subblur;
-
-                subblur = (blur - 1.0f) / 2.0f * (float)(i + 1);
-
-                blurTexScale[0] = blurTexScale[1] = subblur;
-
-                color[0] = color[1] = color[2] = 1.0f;
-                if (i != 0)
-                    color[3] = 1.0f;
-                else
-                    color[3] = 0.5f;
-
-                FBO_Blit(tr.quarterFbo[0], NULL, blurTexScale, tr.quarterFbo[1], NULL, &tr.bokehShader, color, GLS_SRCBLEND_SRC_ALPHA | GLS_DSTBLEND_ONE_MINUS_SRC_ALPHA);
-            }
-
-            FBO_Blit(tr.quarterFbo[1], NULL, NULL, dst, dstBox, NULL, NULL, 0);
-        }
-#endif
     }
 }
